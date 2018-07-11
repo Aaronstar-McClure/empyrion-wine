39afe70a89e5ea29a6b90d090f9b17aa2e552d8a
Aaronstar-McClure <jfeldt19@gmail.com>
2018-07-11 15:20:52 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 5
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server and patch to background
+${MODIFIED_STARTUP} &
+echo "waiting for server to start before connecting telnet..."
+# Waiting for empyrion server to properly start, connecting too soon will cause failure
+sleep 20
+# Have netcat connect to internal telnet, check that is is infact internal, and not accessing an outside the docker port
+nc localhost 30004
